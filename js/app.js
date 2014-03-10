f2a2ac6422b8e2bf42c20bda42d5786eaee8744f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-10 00:48:50 -0700

    created nested route and template

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ App = Ember.Application.create();
 
 App.Router.map(function(){
   this.resource('home');
-  this.resource('kittens');
+  this.resource('kittens', function(){
+    this.resource('kitten', { path: ':kitten_id' });
+  });
   this.resource('puppies');
   this.resource('bunnies');
-  this.resource('kitten', { path: ':kitten_id' });
   this.resource('puppy', { path: ':puppy_id' });
   this.resource('bunny', { path: ':bunny_id' });
 });
