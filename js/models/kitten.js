a04a8ae86a7855849a3ff766484750d1c918d29e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-10 00:29:48 -0700

    add description

--- js/models/kitten.js
+++ js/models/kitten.js
@@ -1,13 +1,17 @@
 var kittens = [{
   id: 1,
   title: "Mew?",
+  description: "Kitty ipsum dolor sit amet, lick shed everywhere faucibus eat suscipit, toss the mousie tincidunt a chuf iaculis sleep in the sink lay down in your way. Aliquam claw knock over the lamp meow hairball quis nunc, jump on the table non nunc run jump on the table. Lick chase the red dot eat the grass dolor chuf, I don't like that food in viverra rutrum pellentesque enim fluffy fur enim ut. Feed me rhoncus hiss vehicula rhoncus sleep in the sink, tempus egestas lick sagittis. Sollicitudin run meow suscipit toss the mousie, suspendisse vehicula tempus pellentesque stuck in a tree eat the grass. Scratched libero sleep on your keyboard scratched mauris a meow, chase the red dot nunc orci turpis vehicula quis. Sunbathe scratched accumsan sagittis chuf vehicula, kittens nam jump on the table pellentesque attack your ankles dolor. Nibh nam enim ut hairball rutrum, bat in viverra justo tail flick climb the curtains tail flick.",
   url: "http://critterbabies.com/wp-content/gallery/kittens/cats-animals-kittens-background-us.jpg"
 }, {
   id: 2,
   title: "Rawr!",
+  description: "Kitty ipsum dolor sit amet, lick shed everywhere faucibus eat suscipit, toss the mousie tincidunt a chuf iaculis sleep in the sink lay down in your way. Aliquam claw knock over the lamp meow hairball quis nunc, jump on the table non nunc run jump on the table. Lick chase the red dot eat the grass dolor chuf, I don't like that food in viverra rutrum pellentesque enim fluffy fur enim ut. Feed me rhoncus hiss vehicula rhoncus sleep in the sink, tempus egestas lick sagittis. Sollicitudin run meow suscipit toss the mousie, suspendisse vehicula tempus pellentesque stuck in a tree eat the grass. Scratched libero sleep on your keyboard scratched mauris a meow, chase the red dot nunc orci turpis vehicula quis. Sunbathe scratched accumsan sagittis chuf vehicula, kittens nam jump on the table pellentesque attack your ankles dolor. Nibh nam enim ut hairball rutrum, bat in viverra justo tail flick climb the curtains tail flick.",
+
   url: "http://critterbabies.com/wp-content/gallery/kittens/Kitten-pic-cute-kittens-16292210-1024-768.jpg"
 }, {
   id: 3,
+  description: "Kitty ipsum dolor sit amet, lick shed everywhere faucibus eat suscipit, toss the mousie tincidunt a chuf iaculis sleep in the sink lay down in your way. Aliquam claw knock over the lamp meow hairball quis nunc, jump on the table non nunc run jump on the table. Lick chase the red dot eat the grass dolor chuf, I don't like that food in viverra rutrum pellentesque enim fluffy fur enim ut. Feed me rhoncus hiss vehicula rhoncus sleep in the sink, tempus egestas lick sagittis. Sollicitudin run meow suscipit toss the mousie, suspendisse vehicula tempus pellentesque stuck in a tree eat the grass. Scratched libero sleep on your keyboard scratched mauris a meow, chase the red dot nunc orci turpis vehicula quis. Sunbathe scratched accumsan sagittis chuf vehicula, kittens nam jump on the table pellentesque attack your ankles dolor. Nibh nam enim ut hairball rutrum, bat in viverra justo tail flick climb the curtains tail flick.",
   title: "Quack?",
   url: "http://images2.fanpop.com/image/photos/10500000/Cute-Wallpapers-cute-kittens-10501750-1024-768.jpg"
 }];
