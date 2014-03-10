6e3a203c420235234a3fe25a15006f49fbc6c79e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-09 23:11:57 -0700

    add links to the fluffy things

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ App = Ember.Application.create();
 
 App.Router.map(function(){
   this.resource('home');
+  this.resource('kittens');
+  this.resource('puppies');
+  this.resource('bunnies');
+
 
 })
