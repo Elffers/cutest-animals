ba9b64060d1e68ff055f38d1caf83d08c2613961
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-09 23:49:32 -0700

    bind kittens model to template

--- js/app.js
+++ js/app.js
@@ -5,6 +5,22 @@ App.Router.map(function(){
   this.resource('kittens');
   this.resource('puppies');
   this.resource('bunnies');
-
-
 })
+
+App.KittensRoute = Ember.Route.extend({
+  model: function() {
+    return kittens;
+  }
+});
+
+
+var kittens = [{
+  title: "Tickles",
+  url: "http://critterbabies.com/wp-content/gallery/kittens/cats-animals-kittens-background-us.jpg"
+}, {
+  title: "Rawr",
+  url: "http://critterbabies.com/wp-content/gallery/kittens/Kitten-pic-cute-kittens-16292210-1024-768.jpg"
+}, {
+  title: "Quack?",
+  url: "http://images2.fanpop.com/image/photos/10500000/Cute-Wallpapers-cute-kittens-10501750-1024-768.jpg"
+}];
